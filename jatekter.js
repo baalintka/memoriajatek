63572bd83477807caa26b111cb8775a4939951fb
Szöllősi <ballint3@gmail.com>
2023-05-18 15:02:49 +0200

    trigger

--- jatekter.js
+++ jatekter.js
@@ -3,7 +3,7 @@ import Kartya from "./kartya.js";
 class Jatekter {
   #kartyalista = [];
   #kivalasztottKartyalista = [];
-
+  
   constructor(kartyalista) {
     const szuloELEM = $("article");
     
@@ -13,7 +13,7 @@ class Jatekter {
     for (let index = 0; index < this.#kartyalista.length; index++) {
       let lapok = new Kartya(this.#kartyalista[index], szuloELEM);
     }
-    this.#kattintasTrigger();
+    szuloELEM.on("click",this.#kattintasTrigger);
   }
   #kever() {
     for (let i = this.#kartyalista.length - 1; i >= 0; i--) {
@@ -24,8 +24,22 @@ class Jatekter {
     }
   }
   #kattintasTrigger(event){
+    let db=0;
     const aktKep = event.target;
     console.log(aktKep.id)
+    console.log($(aktKep).attr("id"));
+    aktKep.src=this.#kartyalista[aktKep.id]
+    this.#kivalasztottKartyalista.push($(aktKep).attr("id"))
+    db++;
+    if (db==2) {
+        db=0;
+        visszafordit();
+    }
+    console.log(this.#kivalasztottKartyalista)
+  }
+  #visszafordit(){
+    console.log(this.#kivalasztottKartyalista)
+
 
   }
 }
